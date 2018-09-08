c889df949c2081ea771c13ccdf8ce6947b3d1c3d
bonecrushereb <bonecrushereb@gmail.com>
2018-09-07 23:11:54 -0700

    setting side menu to pagrent pages

--- localwpfc/wp-content/themes/fictional-university-theme/page.php
+++ localwpfc/wp-content/themes/fictional-university-theme/page.php
@@ -26,6 +26,12 @@
     </div>
       <?php } ?>
       
+      <?php 
+      $testArray = get_pages(array(
+        'child_of' => get_the_ID()
+      ));
+
+      if ($theParent or $testArray) { ?>
       <div class="page-links">
         <h2 class="page-links__title"><a href="<?php echo get_permalink($theParent); ?>"><?php echo get_the_title($theParent); ?></a></h2>
         <ul class="min-list">
@@ -38,12 +44,13 @@
 
             wp_list_pages(array(
               'title_li' => NULL,
-              'child_of' => $findChildrenOf
+              'child_of' => $findChildrenOf,
+              'sort_column' => 'menu_order'
             )); 
           ?>
         </ul>
       </div>
-
+      <?php } ?>
       <div class="generic-content">
         <?php the_content(); ?>
       </div>
