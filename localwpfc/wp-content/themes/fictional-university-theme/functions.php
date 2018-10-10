b732493d4d383cad5275101528b6385f7c2bf207
bonecrushereb <bonecrushereb@gmail.com>
2018-10-09 23:07:24 -0700

    reducing duplicate code

--- localwpfc/wp-content/themes/fictional-university-theme/functions.php
+++ localwpfc/wp-content/themes/fictional-university-theme/functions.php
@@ -1,5 +1,19 @@
 <?php
 
+function pageBanner() {
+  ?>
+   <div class="page-banner">
+      <div class="page-banner__bg-image" style="background-image: url(<?php $pageBannerImage = get_field('page_banner_background_image'); echo $pageBannerImage['sizes']['pageBanner']; ?>);"></div>
+      <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title"><?php the_title() ?></h1>
+        <div class="page-banner__intro">
+          <p><?php the_field('page_banner_subtitle'); ?></p>
+        </div>
+      </div>  
+    </div>
+  <?php 
+}
+
 function university_files() {
   wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, microtime(), true);
   wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
@@ -17,6 +31,7 @@ function university_features() {
   add_theme_support('post-thumbnails');
   add_image_size('professorLandscape', 400, 260, true);
   add_image_size('professorPortrait', 480, 650, true);
+  add_image_size('pageBanner', 1500, 350, true);
 }
 
 add_action('after_setup_theme', 'university_features');
