f12f2958b309da722969399141d3996bf55202ba
bonecrushereb <bonecrushereb@gmail.com>
2018-10-17 23:30:38 -0700

    add relationship between campuses and programs

--- localwpfc/wp-content/themes/fictional-university-theme/archive-campus.php
+++ localwpfc/wp-content/themes/fictional-university-theme/archive-campus.php
@@ -14,12 +14,12 @@
             the_post(); 
             $mapLocation = get_field('map_location');
             ?>
-            <div class="marker" data-lat="<?php echo $mapLocation['lat'];?>" data-lng="<?php echo $mapLocation['lng']; ?>"></div>
-         <?php 
-          }
-         ?>
+            <div class="marker" data-lat="<?php echo $mapLocation['lat'];?>" data-lng="<?php echo $mapLocation['lng']; ?>">
+              <h3><a href="<?php the_permalink(); ?>"><?php echo the_title(); ?></a></h3>
+              <?php echo $mapLocation['address'] ?>
+            </div>
+         <?php } ?>
       </div>   
-
     </div>
 
 <?php get_footer(); ?>
