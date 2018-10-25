e7c8e9a9cc48d08e76588762fd5742a2f97e015b
bonecrushereb <bonecrushereb@gmail.com>
2018-10-24 20:30:51 -0700

    add dynamically created html from search results

--- localwpfc/wp-content/themes/fictional-university-theme/js/modules/Search.js
+++ localwpfc/wp-content/themes/fictional-university-theme/js/modules/Search.js
@@ -24,19 +24,23 @@ class Search {
 
   typingLogic() {
     const url = 'http://localhost:3000/localwpfc/wp-json/wp/v2/posts?search=' + this.searchField.val();
-
     if (this.searchField.val() != this.prevValue) {
       clearTimeout(this.typingTimer);
+
       if (this.searchField.val()) {
         if (!this.isSpinnerVisible) {
           this.searchResults.html('<div class="spinner-loader"></div>');
           this.isSpinnerVisible = true;
         }
-        this.typingTimer = setTimeout(function () {
-          $.getJSON(url, function(data) {
-            console.log(data[0].title.rendered);
-          });
-          this.isSpinnerVisible = false;
+        this.typingTimer = setTimeout(function() {
+          $.getJSON(url, data => {
+            this.searchResults.html(`
+              <h2 class="search-overlay__section-title">General Information</h2>
+              <ul class="linked-list min-list">
+                ${data.map(data => `<li><a href="${data.link}">${data.title.rendered}</a></li>`).join('')}
+              </ul>
+              `);
+          }); 
         }.bind(this), 2000);
       } else {
         this.searchResults.html('');
