57291dc4dc7379236c2210a4442920ae6995b302
bonecrushereb <bonecrushereb@gmail.com>
2018-10-25 08:14:02 -0700

    move search html to js file

--- localwpfc/wp-content/themes/fictional-university-theme/js/modules/Search.js
+++ localwpfc/wp-content/themes/fictional-university-theme/js/modules/Search.js
@@ -2,6 +2,7 @@ import $ from 'jquery';
 
 class Search {
   constructor() {
+    this.addSearch();
     this.searchResults = $("#search-overlay__results");
     this.openButton = $(".js-search-trigger");
     this.closeButton = $(".search-overlay__close");
@@ -41,7 +42,7 @@ class Search {
               `);
             this.isSpinnerVisible = false;
           }); 
-        }.bind(this), 2000);
+        }.bind(this), 375);
       } else {
         this.searchResults.html('');
         this.isSpinnerVisible = false;
@@ -68,6 +69,24 @@ class Search {
     this.searchOverlay.removeClass("search-overlay--active");
     $("body").removeClass("body-no-scroll");
     this.isOverlayOpen = false;
+  }
+
+  addSearch() {
+    $("body").append(`
+    <div class="search-overlay">
+      <div class="search-overlay__top">
+        <div class="container">
+          <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+          <input type="text" class="search-term" placeholder="What are you looking for" id="search-term">
+          <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
+        </div>
+      </div>
+
+      <div class="container">
+        <div id="search-overlay__results"></div>
+      </div>
+    </div>
+    `);
   }
 }
 
