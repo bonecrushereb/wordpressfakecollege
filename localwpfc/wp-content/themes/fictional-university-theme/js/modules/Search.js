ff1e5f87681c6a4efc60785d94ffc5577a2bee0d
bonecrushereb <bonecrushereb@gmail.com>
2018-10-26 23:39:47 -0700

    add async programming to search

--- localwpfc/wp-content/themes/fictional-university-theme/js/modules/Search.js
+++ localwpfc/wp-content/themes/fictional-university-theme/js/modules/Search.js
@@ -24,7 +24,8 @@ class Search {
 
 
   typingLogic() {
-    const url = universityData.root_url + '/wp-json/wp/v2/posts?search=' + this.searchField.val();
+    const postUrl = universityData.root_url + '/wp-json/wp/v2/posts?search=' + this.searchField.val();
+    const pageUrl = universityData.root_url + '/wp-json/wp/v2/pages?search=' + this.searchField.val();
     if (this.searchField.val() != this.prevValue) {
       clearTimeout(this.typingTimer);
 
@@ -34,14 +35,18 @@ class Search {
           this.isSpinnerVisible = true;
         }
         this.typingTimer = setTimeout(function() {
-          $.getJSON(url, data => {
-            this.searchResults.html(`
-              <h2 class="search-overlay__section-title">General Information</h2>
-              ${data.length ? '<ul class="linked-list min-list">': '<p>No General Information found.</p>'}
-                ${data.map(data => `<li><a href="${data.link}">${data.title.rendered}</a></li>`).join('')}
+          $.when(
+            $.getJSON(postUrl), 
+            $.getJSON(pageUrl)
+            ).then((posts,pages) => {
+              const combinedResults = posts[0].concat(pages[0]);
+              this.searchResults.html(`
+                <h2 class="search-overlay__section-title">General Information</h2>
+                ${combinedResults.length ? '<ul class="linked-list min-list">': '<p>No General Information found.</p>'}
+                ${combinedResults.map(data => `<li><a href="${data.link}">${data.title.rendered}</a></li>`).join('')}
               `);
-            this.isSpinnerVisible = false;
-          }); 
+              this.isSpinnerVisible = false;
+          });
         }.bind(this), 375);
       } else {
         this.searchResults.html('');
