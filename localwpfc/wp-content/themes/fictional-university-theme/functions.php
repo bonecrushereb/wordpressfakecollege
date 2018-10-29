5634224a9e9cc7cd341ab67fd213cba4115a5f0b
bonecrushereb <bonecrushereb@gmail.com>
2018-10-28 18:21:50 -0700

    added author to search

--- localwpfc/wp-content/themes/fictional-university-theme/functions.php
+++ localwpfc/wp-content/themes/fictional-university-theme/functions.php
@@ -1,9 +1,9 @@
 <?php
 
 function university_custom_rest() {
-  register_rest_field('posts', 'authorName', array(
+  register_rest_field('post', 'authorName', array(
     'get_callback' => function() { return get_the_author(); }
-  ));  
+  ));
 }
 
 add_action('rest_api_init', 'university_custom_rest');
