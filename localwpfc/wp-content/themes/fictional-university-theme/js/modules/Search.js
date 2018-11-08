6d44bed38b99f287e29573a84a2db15371042b7a
bonecrushereb <bonecrushereb@gmail.com>
2018-11-08 12:02:49 -0800

    finish basic search for non javascript users

--- localwpfc/wp-content/themes/fictional-university-theme/js/modules/Search.js
+++ localwpfc/wp-content/themes/fictional-university-theme/js/modules/Search.js
@@ -111,6 +111,7 @@ class Search {
     this.searchField.val('');
     setTimeout(() => this.searchField.focus(), 301);
     this.isOverlayOpen = true;
+    return false;
   }
 
   closeOverlay() {
@@ -125,7 +126,7 @@ class Search {
       <div class="search-overlay__top">
         <div class="container">
           <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
-          <input type="text" class="search-term" placeholder="What are you looking for" id="search-term">
+          <input type="text" class="search-term" placeholder="What are you looking for?" id="search-term">
           <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
         </div>
       </div>
