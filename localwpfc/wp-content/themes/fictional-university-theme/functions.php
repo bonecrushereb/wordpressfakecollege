6c22cacfb95c7df2b4011294f4a52525aab91f36
bonecrushereb <bonecrushereb@gmail.com>
2018-11-14 10:19:23 -0800

    customized login with title

--- localwpfc/wp-content/themes/fictional-university-theme/functions.php
+++ localwpfc/wp-content/themes/fictional-university-theme/functions.php
@@ -131,3 +131,23 @@ function adminBar () {
 }
 add_action('admin_init', 'redirectFrontEnd');
 
+add_filter('login_headerurl', 'headerUrl');
+
+function headerUrl() {
+  return esc_url(site_url('/'));
+}
+
+add_action('login_enqueue_scripts', 'loginCSS');
+
+function loginCSS() {
+  wp_enqueue_style('university_main_styles', get_stylesheet_uri());
+  wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+
+}
+
+add_filter('login_headertitle', 'loginTitle');
+
+function loginTitle() {
+  return get_bloginfo('name');
+}
+
