06b629ffa81062f3bb299998d395fcc0208e63a4
bonecrushereb <bonecrushereb@gmail.com>
2018-11-15 11:58:49 -0800

    adding conditional for notes as well as started notes page

---
+++ localwpfc/wp-content/themes/fictional-university-theme/page-my-notes.php
@@ -0,0 +1,19 @@
+<?php
+
+  if (!is_user_logged_in()) {
+    wp_redirect(esc_url(site_url('/')));
+    exit;
+  }
+
+  get_header();
+
+  while(have_posts()) {
+    the_post(); 
+    pageBanner();
+?>
+
+  <div class="container container--narrow page-section">
+
+  </div>
+    
+  <?php } get_footer(); ?>
