2be2e9f2a200d80ffe092415eaaa9ac0a5fec5ef
bonecrushereb <bonecrushereb@gmail.com>
2018-11-15 12:08:17 -0800

    add notes post-type

--- localwpfc/wp-content/mu-plugins/university-post-types.php
+++ localwpfc/wp-content/mu-plugins/university-post-types.php
@@ -53,7 +53,7 @@ function university_post_types() {
   ),
   'menu_icon' => 'dashicons-awards'
 
-));
+  ));
 
   register_post_type('professor', array(
   'show_in_rest' => true,
@@ -67,8 +67,22 @@ function university_post_types() {
     'singular_name' => 'Professor'
   ),
   'menu_icon' => 'dashicons-welcome-learn-more'
+  ));  
 
-));
+  register_post_type('note', array(
+  'show_in_rest' => true,
+  'supports' => array('title', 'editor'),
+  'public' => false,
+  'show_ui' => true,
+  'labels' => array(
+    'name' => 'Notes',
+    'add_new_item' => 'Add New Note',
+    'edit_item' => 'Edit Note',
+    'add_items' => 'All Notes',
+    'singular_name' => 'Note'
+  ),
+  'menu_icon' => 'dashicons-welcome-write-blog'
+  ));
 }
 
 add_action('init', 'university_post_types');  
