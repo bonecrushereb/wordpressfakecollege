029ae72164f62cb0950417bb524e8b9c616a57eb
bonecrushereb <bonecrushereb@gmail.com>
2018-11-19 13:58:22 -0800

    make note editible or not

--- localwpfc/wp-content/themes/fictional-university-theme/js/modules/MyNotes.js
+++ localwpfc/wp-content/themes/fictional-university-theme/js/modules/MyNotes.js
@@ -2,40 +2,57 @@ import $ from 'jquery';
 
 class MyNotes {
   constructor() {
-    this.deleteNote = $('.delete-note');
-    this.editNote = $('.edit-note');
     this.events();
   }
 
-  events() {
-    this.deleteNote.on("click", (e) => {
-      const thisNote = $(e.target).parents('li');
-      const noteUrl = universityData.root_url + '/wp-json/wp/v2/note/' + thisNote.data('id');
-      if(confirm('Are you sure you want to delete ' + $('input').val() + '?')) { 
-        $.ajax({
-        beforeSend: (xhr) => {
-          xhr.setRequestHeader('X-WP-Nonce', universityData.nonce);
-        },
-        url: noteUrl,
-        type: 'DELETE',
-        success: (response) => {
-          thisNote.slideUp();
-        },
-        error: (response) => {
-          console.log(response);
-        }
-        });
-      } else {
-        return false;
-      }
-    });
+    events() {
+    $(".delete-note").on("click", this.deleteNote);
+    $(".edit-note").on("click", this.editNote.bind(this));
+  }
 
-    this.editNote.on("click", (e) => {
-      const thisNote = $(e.target).parents('li');
-      thisNote.find('.note-title-field, .note-body-field').removeAttr('readonly').addClass('note-active-field');
-      thisNote.find('.update-note').addClass('update-note--visible');
-    });
+    editNote(e) {
+    var thisNote = $(e.target).parents("li");
+    if (thisNote.data("state") == "editable") {
+      this.noteReadOnly(thisNote);
+    } else {
+      this.noteEditable(thisNote);
+    }
+  }
+
+  noteEditable(thisNote) {
+    thisNote.find(".edit-note").html('<i class="fa fa-times" aria-hidden="true"></i> Cancel');
+    thisNote.find(".note-title-field, .note-body-field").removeAttr("readonly").addClass("note-active-field");
+    thisNote.find(".update-note").addClass("update-note--visible");
+    thisNote.data("state", "editable");
+  }
+
+  noteReadOnly(thisNote) {
+    thisNote.find(".edit-note").html('<i class="fa fa-pencil" aria-hidden="true"></i> Edit');
+    thisNote.find(".note-title-field, .note-body-field").attr("readonly", "readonly").removeClass("note-active-field");
+    thisNote.find(".update-note").removeClass("update-note--visible");
+    thisNote.data("state", "cancel");
+  }
+
+  deleteNote(e) {
+    const thisNote = $(e.target).parents('li');
+    const noteUrl = universityData.root_url + '/wp-json/wp/v2/note/' + thisNote.data('id');
+    if(confirm('Are you sure you want to delete ' + $('input').val() + '?')) { 
+      $.ajax({
+      beforeSend: (xhr) => {
+        xhr.setRequestHeader('X-WP-Nonce', universityData.nonce);
+      },
+      url: noteUrl,
+      type: 'DELETE',
+      success: (response) => {
+        thisNote.slideUp();
+      },
+      error: (response) => {
+        console.log(response);
+      }
+      });
+    } else {
+      return false;
+    }
   }
 }
-
 export default MyNotes;
